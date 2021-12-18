4ec840024d3f692a4da01fe1e76f14115acfa17d
PlasticBucket1944 <Orangina2739@gmail.com>
2021-12-18 23:31:49 +0900

    選択日のセルを青色を表示するように

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,6 +1,11 @@
 $(function() {
+    let selectedDay = $('.today'); // 選択日保持用変数
+    $(selectedDay).addClass('selected-day');
+
+    // 表示月の日付クリック時イベント
     $('.current-month').click(function() {
-        console.log(this);
+        $(selectedDay).removeClass('selected-day');
+        $(this).addClass('selected-day');
+        selectedDay = $(this);
     });
-    console.log("fire!");
 });
