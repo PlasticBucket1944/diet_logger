3c8a326d47bfd49755712302deab28c33ebf3f5d
PlasticBucket1944 <Orangina2739@gmail.com>
2022-01-05 19:59:38 +0900

    modal.jsのコメント追加

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,10 +1,10 @@
 $(function() {
     $('.js-modal-open').on('click',function() {
         $('.js-modal').fadeIn();
-        return false;
+        return false; // aタグを無効にする
     });
     $('.js-modal-close').on('click',function() {
         $('.js-modal').fadeOut();
-        return false;
+        return false; // aタグを無効にする
     });
 });
